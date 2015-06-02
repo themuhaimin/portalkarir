dc699a43e0ced374d5bab58fe78f5bb454349d4a
Dwi Prihandi <dwi_prihandi@yahoo.com>
2015-06-02 00:21:20 +0000

    adding puma again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root 'static_pages#beranda'
-
   get 'tentang' => 'static_pages#tentang'
 
   # The priority is based upon order of creation: first created -> highest priority.
