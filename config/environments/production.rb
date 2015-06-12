58d14df0effb0218e677d37bddf6185d9f35ce74
Dwi Prihandi <dwi_prihandi@yahoo.com>
2015-06-12 03:17:09 +0000

    Add profil

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,10 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.cache_classes = true 
+  config.serve_static_assets = true 
+  config.assets.compile = true 
+  config.assets.digest = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
